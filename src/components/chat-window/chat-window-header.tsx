628ffcff2b2022c2b9bcbc2bf202eaa4fd824ea4
Me Sailor <74054075+Mesailor@users.noreply.github.com>
2025-10-28 09:23:30 +0400

    feat. basic header ui

--- src/components/chat-window/chat-window-header.tsx
+++ src/components/chat-window/chat-window-header.tsx
@@ -1,3 +1,60 @@
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Button } from "../ui/button";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import {
+  MoreHorizontalIcon,
+  PhoneIcon,
+  SearchIcon,
+  VideoIcon,
+} from "lucide-react";
+
 export function ChatWindowHeader() {
-  return <div className="sticky top-0 bg-zinc-400 z-10">Header</div>;
+  return (
+    <div
+      className={cn(
+        "sticky top-0 border-b bg-background z-10 p-2",
+        "flex items-center gap-4"
+      )}
+    >
+      <div className="flex items-center gap-2">
+        <Avatar className="rounded-full">
+          <AvatarImage
+            src="https://github.com/evilrabbit.png"
+            alt="@evilrabbit"
+          />
+          <AvatarFallback>ER</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col">
+          <span className="font-medium">Evil Rabbit</span>
+        </div>
+      </div>
+      <div className="flex-1 flex items-center gap-2">
+        <span className="text-sm font-semibold">AKA</span>
+        <span className="text-sm font-medium">Chocolate Bunny</span>
+      </div>
+      <div className="flex items-center gap-1.5">
+        {/* Additional header actions can be added here */}
+        <InputGroup className="sm:flex hidden">
+          <InputGroupInput placeholder="Search..." />
+          <InputGroupAddon>
+            <SearchIcon />
+          </InputGroupAddon>
+        </InputGroup>
+        <Button variant="ghost" className="size-8 sm:inline-flex hidden">
+          <PhoneIcon />
+        </Button>
+        <Button variant="ghost" className="size-8 sm:inline-flex hidden">
+          <VideoIcon />
+        </Button>
+        <Button variant="ghost" className="size-8">
+          <MoreHorizontalIcon />
+        </Button>
+      </div>
+    </div>
+  );
 }
