758cca69c63d39cb8593d91f1e6af51fe1deaca0
Me Sailor <74054075+Mesailor@users.noreply.github.com>
2025-11-06 10:02:21 +0400

    feat. basic messages ui

--- src/components/chat-window/chat-window-messages.tsx
+++ src/components/chat-window/chat-window-messages.tsx
@@ -1,11 +1,152 @@
-import { Skeleton } from "../ui/skeleton";
+import { cn } from "@/lib/utils";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { Separator } from "../ui/separator";
+import { Fragment, ReactNode } from "react";
+import { MESSAGES } from "@/data/messages";
 
 export function ChatWindowMessages() {
   return (
-    <div className="flex-1 flex flex-col gap-1">
-      {Array.from({ length: 100 }, (_, i) => (
-        <Skeleton key={i} className="h-5 w-full rounded-md" />
-      ))}
+    <div className="flex-1 flex flex-col-reverse overflow-auto p-2">
+      {MESSAGES.map((msg, i, msgs) => {
+        // If date changed, show date item
+        if (
+          new Date(msg.timestamp).toDateString() !==
+          new Date(msgs[i + 1]?.timestamp).toDateString()
+        ) {
+          return (
+            <Fragment key={msg.id}>
+              <PrimaryMessage
+                avatarSrc={msg.sender.avatarUrl}
+                avatarAlt={msg.sender.username}
+                avatarFallback={msg.sender.name.slice(0, 2)}
+                senderName={msg.sender.name}
+                content={msg.content}
+                timestamp={msg.timestamp}
+              />
+              <DateItem timestamp={msg.timestamp} className="my-4" />
+            </Fragment>
+          );
+        }
+
+        // If next item is same user, show additional
+        if (msg.sender.id === msgs[i + 1]?.sender.id) {
+          return (
+            <AdditionalMessage
+              key={msg.id}
+              content={msg.content}
+              timestamp={msg.timestamp}
+            />
+          );
+        }
+        // Else, show primary
+        else {
+          return (
+            <PrimaryMessage
+              className="mt-4"
+              key={msg.id}
+              avatarSrc={msg.sender.avatarUrl}
+              avatarAlt={msg.sender.username}
+              avatarFallback={msg.sender.name.slice(0, 2)}
+              senderName={msg.sender.name}
+              content={msg.content}
+              timestamp={msg.timestamp}
+            />
+          );
+        }
+      })}
+    </div>
+  );
+}
+
+function PrimaryMessage({
+  avatarSrc,
+  avatarAlt,
+  avatarFallback,
+  senderName,
+  content,
+  timestamp,
+
+  className,
+}: {
+  avatarSrc?: string;
+  avatarAlt?: string;
+  avatarFallback?: string;
+  senderName: string;
+  content: ReactNode;
+  timestamp: number;
+
+  className?: string;
+}) {
+  return (
+    <div
+      className={cn(
+        "shrink-0 flex gap-2 hover:bg-accent rounded-md px-2",
+        className
+      )}
+    >
+      <Avatar className="rounded-full size-10 mt-1 mx-1">
+        <AvatarImage src={avatarSrc} alt={avatarAlt} />
+        <AvatarFallback>{avatarFallback}</AvatarFallback>
+      </Avatar>
+      <div className="flex flex-col">
+        <div className="flex items-baseline gap-2">
+          <p className="font-medium">{senderName}</p>
+          <p className="text-xs text-muted-foreground">
+            {/* 07.09.2020, 16:28 */}
+            {new Intl.DateTimeFormat("en-US", {
+              dateStyle: "medium",
+              timeStyle: "short",
+            }).format(timestamp)}
+          </p>
+        </div>
+        <p>{content}</p>
+      </div>
+    </div>
+  );
+}
+
+function AdditionalMessage({
+  content,
+  timestamp,
+}: {
+  content: ReactNode;
+  timestamp: number;
+}) {
+  return (
+    <div className="group/same-user-message shrink-0 flex gap-2 hover:bg-accent rounded-md px-2 py-0.5">
+      <div
+        className={cn(
+          "shrink-0 w-12 text-[10px] text-muted-foreground text-right leading-6",
+          "group-hover/same-user-message:visible invisible"
+        )}
+      >
+        {/* 16:08 */}
+        {new Intl.DateTimeFormat("en-US", {
+          timeStyle: "short",
+        }).format(timestamp)}
+      </div>
+      <span>{content}</span>
+    </div>
+  );
+}
+
+function DateItem({
+  timestamp,
+  className,
+}: {
+  timestamp: number;
+  className?: string;
+}) {
+  return (
+    <div className={cn("px-2 flex items-center gap-1", className)}>
+      <Separator className="flex-1" />
+      <span className="text-muted-foreground text-xs font-semibold min-w-max">
+        {/* 31 October 2025 */}
+        {new Intl.DateTimeFormat("en-US", {
+          dateStyle: "long",
+        }).format(timestamp)}
+      </span>
+      <Separator className="flex-1" />
     </div>
   );
 }
---
+++ src/data/messages.ts
@@ -0,0 +1,187 @@
+export const MESSAGES = [
+  {
+    id: 16,
+    sender: {
+      id: "evilrabbit-user-id",
+      name: "Evil Rabbit",
+      avatarUrl: "https://github.com/evilrabbit.png",
+      username: "@evilrabbit",
+    },
+    timestamp: 1234878110123,
+    content:
+      "But could you also provide me with the updated documentation? I want to make sure I have all the latest information before proceeding.",
+  },
+  {
+    id: 15,
+    sender: {
+      id: "evilrabbit-user-id",
+      name: "Evil Rabbit",
+      avatarUrl: "https://github.com/evilrabbit.png",
+      username: "@evilrabbit",
+    },
+    timestamp: 1234878100123,
+    content:
+      "I've reviewed the changes, and everything looks perfect! You did a fantastic job implementing the new feature.",
+  },
+  {
+    id: 14,
+    sender: {
+      id: "evilrabbit-user-id",
+      name: "Evil Rabbit",
+      avatarUrl: "https://github.com/evilrabbit.png",
+      username: "@evilrabbit",
+    },
+    timestamp: 1234678090123,
+    content:
+      "Great! I'll take a look at it and get back to you with my feedback shortly. Thanks for the update!",
+  },
+  {
+    id: 13,
+    sender: {
+      id: "shadcn-user-id",
+      name: "Shadcn",
+      avatarUrl: "https://github.com/shadcn.png",
+      username: "@shadcn",
+    },
+    timestamp: 1234678080123,
+    content:
+      "But before that, could you please review the changes I've made and let me know if everything looks good?",
+  },
+  {
+    id: 12,
+    sender: {
+      id: "shadcn-user-id",
+      name: "Shadcn",
+      avatarUrl: "https://github.com/shadcn.png",
+      username: "@shadcn",
+    },
+    timestamp: 1234678070123,
+    content:
+      "Hey! Just wanted to let you know that I've finished the task. It's ready for review.",
+  },
+  {
+    id: 11,
+    sender: {
+      id: "evilrabbit-user-id",
+      name: "Evil Rabbit",
+      avatarUrl: "https://github.com/evilrabbit.png",
+      username: "@evilrabbit",
+    },
+    timestamp: 1234677970123,
+    content:
+      "And how is the project going? Are there any blockers or things I can help with?",
+  },
+  {
+    id: 10,
+    sender: {
+      id: "evilrabbit-user-id",
+      name: "Evil Rabbit",
+      avatarUrl: "https://github.com/evilrabbit.png",
+      username: "@evilrabbit",
+    },
+    timestamp: 1234677910123,
+    content:
+      "Hey! How are you? Did you get all the information you needed to proceed with the implementation?",
+  },
+  {
+    id: 9,
+    sender: {
+      id: "shadcn-user-id",
+      name: "Shadcn",
+      avatarUrl: "https://github.com/shadcn.png",
+      username: "@shadcn",
+    },
+    timestamp: 1234567919123,
+    content: "Once I'm done, I'll notify you in this chat",
+  },
+  {
+    id: 8,
+    sender: {
+      id: "shadcn-user-id",
+      name: "Shadcn",
+      avatarUrl: "https://github.com/shadcn.png",
+      username: "@shadcn",
+    },
+    timestamp: 1234567917123,
+    content: "Now I can start working",
+  },
+  {
+    id: 7,
+    sender: {
+      id: "shadcn-user-id",
+      name: "Shadcn",
+      avatarUrl: "https://github.com/shadcn.png",
+      username: "@shadcn",
+    },
+    timestamp: 1234567913123,
+    content:
+      "Awesome! Thank you very much! That clarifies everything! It's a bit tricky, but I think I got it now. And I can proceed with the implementation.",
+  },
+  {
+    id: 6,
+    sender: {
+      id: "evilrabbit-user-id",
+      name: "Evil Rabbit",
+      avatarUrl: "https://github.com/evilrabbit.png",
+      username: "@evilrabbit",
+    },
+    timestamp: 1234567910123,
+    content:
+      "Additional details - details are very additional, but if you want to add it, you have detalized it first. Otherwise the addition could fail. In case detalization fails, you probably won't be able to add the details",
+  },
+  {
+    id: 5,
+    sender: {
+      id: "evilrabbit-user-id",
+      name: "Evil Rabbit",
+      avatarUrl: "https://github.com/evilrabbit.png",
+      username: "@evilrabbit",
+    },
+    timestamp: 1234567910123,
+    content: "Here you go:",
+  },
+  {
+    id: 4,
+    sender: {
+      id: "evilrabbit-user-id",
+      name: "Evil Rabbit",
+      avatarUrl: "https://github.com/evilrabbit.png",
+      username: "@evilrabbit",
+    },
+    timestamp: 1234567899123,
+    content: "Of course! Let me find it first",
+  },
+  {
+    id: 3,
+    sender: {
+      id: "evilrabbit-user-id",
+      name: "Evil Rabbit",
+      avatarUrl: "https://github.com/evilrabbit.png",
+      username: "@evilrabbit",
+    },
+    timestamp: 1234567895123,
+    content: "Hey! I'm good, thank you!",
+  },
+  {
+    id: 2,
+    sender: {
+      id: "shadcn-user-id",
+      name: "Shadcn",
+      avatarUrl: "https://github.com/shadcn.png",
+      username: "@shadcn",
+    },
+    timestamp: 1234567892123,
+    content: "Could you provide me with some additional details?",
+  },
+  {
+    id: 1,
+    sender: {
+      id: "shadcn-user-id",
+      name: "Shadcn",
+      avatarUrl: "https://github.com/shadcn.png",
+      username: "@shadcn",
+    },
+    timestamp: 1234567890123,
+    content: "Hello, how are you?",
+  },
+];
