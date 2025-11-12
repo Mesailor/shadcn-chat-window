b21e374c492230b035fa2f62b1841d7244c89217
Me Sailor <74054075+Mesailor@users.noreply.github.com>
2025-11-12 10:04:30 +0400

    feat. split header into components

--- src/components/chat-window/chat-window-example-component.tsx
+++ src/components/chat-window/chat-window-example-component.tsx
@@ -1,12 +1,71 @@
+import {
+  MoreHorizontalIcon,
+  PhoneIcon,
+  SearchIcon,
+  VideoIcon,
+} from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "../ui/input-group";
 import { ChatWindow } from "./chat-window";
-import { ChatWindowHeader } from "./chat-window-header";
+import {
+  ChatWindowHeader,
+  ChatWindowHeaderMain,
+  ChatWindowHeaderEnd,
+  ChatWindowHeaderStart,
+} from "./chat-window-header";
 import { ChatWindowInput } from "./chat-window-input";
 import { ChatWindowMessages } from "./chat-window-messages";
+import { Button } from "../ui/button";
 
 export function ChatWindowExampleComponent() {
   return (
     <ChatWindow>
-      <ChatWindowHeader />
+      <ChatWindowHeader className="border-b">
+        <ChatWindowHeaderStart>
+          <Avatar className="rounded-full size-6">
+            <AvatarImage
+              src="https://github.com/evilrabbit.png"
+              alt="@evilrabbit"
+            />
+            <AvatarFallback>ER</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col">
+            <span className="font-medium">Evil Rabbit</span>
+          </div>
+        </ChatWindowHeaderStart>
+        <ChatWindowHeaderMain>
+          <span className="text-sm font-semibold">AKA</span>
+          <span className="text-sm font-medium">Chocolate Bunny</span>
+        </ChatWindowHeaderMain>
+        <ChatWindowHeaderEnd>
+          <InputGroup className="@2xl/chat-window:flex hidden">
+            <InputGroupInput placeholder="Search..." />
+            <InputGroupAddon>
+              <SearchIcon />
+            </InputGroupAddon>
+          </InputGroup>
+          <Button
+            variant="ghost"
+            className="size-8 @2xl/chat-window:inline-flex hidden"
+          >
+            <PhoneIcon />
+          </Button>
+          <Button
+            variant="ghost"
+            className="size-8 @2xl/chat-window:inline-flex hidden"
+          >
+            <VideoIcon />
+          </Button>
+          <Button variant="ghost" className="size-8">
+            <MoreHorizontalIcon />
+          </Button>
+        </ChatWindowHeaderEnd>
+      </ChatWindowHeader>
+
       <ChatWindowMessages />
       <ChatWindowInput />
     </ChatWindow>
--- src/components/chat-window/chat-window-header.tsx
+++ src/components/chat-window/chat-window-header.tsx
@@ -1,66 +1,58 @@
-import { cn } from "@/lib/utils";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { Button } from "../ui/button";
-import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupInput,
-} from "@/components/ui/input-group";
-import {
-  MoreHorizontalIcon,
-  PhoneIcon,
-  SearchIcon,
-  VideoIcon,
-} from "lucide-react";
+"use client";
 
-export function ChatWindowHeader() {
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export function ChatWindowHeader({
+  children,
+  className,
+  ...props
+}: { children?: React.ReactNode } & React.ComponentProps<"div">) {
   return (
     <div
       className={cn(
-        "sticky top-0 border-b bg-background z-10 p-2",
-        "flex items-center gap-4"
+        "sticky top-0 z-10 p-2 bg-background flex items-center gap-4",
+        className
       )}
+      {...props}
     >
-      <div className="flex items-center gap-2">
-        <Avatar className="rounded-full size-6">
-          <AvatarImage
-            src="https://github.com/evilrabbit.png"
-            alt="@evilrabbit"
-          />
-          <AvatarFallback>ER</AvatarFallback>
-        </Avatar>
-        <div className="flex flex-col">
-          <span className="font-medium">Evil Rabbit</span>
-        </div>
-      </div>
-      <div className="flex-1 flex items-center gap-2">
-        <span className="text-sm font-semibold">AKA</span>
-        <span className="text-sm font-medium">Chocolate Bunny</span>
-      </div>
-      <div className="flex items-center gap-1.5">
-        {/* Additional header actions can be added here */}
-        <InputGroup className="@2xl/chat-window:flex hidden">
-          <InputGroupInput placeholder="Search..." />
-          <InputGroupAddon>
-            <SearchIcon />
-          </InputGroupAddon>
-        </InputGroup>
-        <Button
-          variant="ghost"
-          className="size-8 @2xl/chat-window:inline-flex hidden"
-        >
-          <PhoneIcon />
-        </Button>
-        <Button
-          variant="ghost"
-          className="size-8 @2xl/chat-window:inline-flex hidden"
-        >
-          <VideoIcon />
-        </Button>
-        <Button variant="ghost" className="size-8">
-          <MoreHorizontalIcon />
-        </Button>
-      </div>
+      {children}
+    </div>
+  );
+}
+
+export function ChatWindowHeaderStart({
+  children,
+  className,
+  ...props
+}: { children?: React.ReactNode } & React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex items-center gap-2", className)} {...props}>
+      {children}
+    </div>
+  );
+}
+
+export function ChatWindowHeaderMain({
+  children,
+  className,
+  ...props
+}: { children?: React.ReactNode } & React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex-1 flex items-center gap-2", className)} {...props}>
+      {children}
+    </div>
+  );
+}
+
+export function ChatWindowHeaderEnd({
+  children,
+  className,
+  ...props
+}: { children?: React.ReactNode } & React.ComponentProps<"div">) {
+  return (
+    <div className={cn("flex items-center gap-2", className)} {...props}>
+      {children}
     </div>
   );
 }
